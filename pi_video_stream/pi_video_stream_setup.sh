6fb8b29e903c83e26998ff60117096482635f1ac
Alice-Xiong <alicexxx0516@gmail.com>
2020-02-18 15:34:57 -0800

    pi local fixed

--- pi_video_stream/pi_video_stream_setup.sh
+++ pi_video_stream/pi_video_stream_setup.sh
@@ -2,9 +2,9 @@
 #$ sudo ./pi_video_stream_setup.sh
 
     echo "Installing python packages"
-    pip python3 install numpy
-    pip python3 install picamera
-    pip python3 install datetime
+    pip3 install imutils
+    pip3 install cv2
+    pip3 install datetime
     pip python3 install tables
     pip python3 install csv
     pip python3 install configparser
