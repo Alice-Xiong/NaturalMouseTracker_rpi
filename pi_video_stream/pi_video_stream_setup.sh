99bbc54b5b92c49a5b323e9c46d70a8de78020bd
Alice-Xiong <alicexxx0516@gmail.com>
2020-02-18 15:36:30 -0800

    updated setup script

--- pi_video_stream/pi_video_stream_setup.sh
+++ pi_video_stream/pi_video_stream_setup.sh
@@ -2,11 +2,12 @@
 #$ sudo ./pi_video_stream_setup.sh
 
     echo "Installing python packages"
-    pip python3 install numpy
-    pip python3 install picamera
-    pip python3 install datetime
-    pip python3 install tables
-    pip python3 install csv
-    pip python3 install configparser
+    pip3 install numpy
+    pip3 install imutils
+    apt install libatlas3-base libsz2 libharfbuzz0b libtiff5 libjasper1 libilmbase12 libopenexr22 libilmbase12 libgstreamer1.0-0 libavcodec57 libavformat57 libavutil55 libswscale4 libqtgui4 libqt4-test libqtcore4
+    pip3 install opencv-contrib-python libwebp6
+    pip3 install wiringpi
+    pip3 install tables
 
+    echo "Install complete\n Make sure you manually ENABLE CAMERA PORT."
     exit
